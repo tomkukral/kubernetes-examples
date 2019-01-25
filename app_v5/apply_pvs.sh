9d9da9d6036919e0a7140cc2bfd42450044ffa7a
Tomáš Kukrál <tomkukral@users.noreply.github.com>
2019-01-25 17:53:40 +0100

    use deployment instead of statefulset

--- app_v5/apply_pvs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for i in {0..10}; do
-	cat pv.yml | sed "s/-0/-$i/g" | kubectl apply -f -
-done
--- app_v5/create_gke_disks.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-size="1GB"
-zone="europe-west1-b"
-for i in {0..10}; do
-	gcloud compute disks create --size "$size" --zone "$zone" "disk-$i"
-done
