ba69897708984ebc09223daf55354f20e95ce6be
Tomáš Kukrál <tom+noreply@6shore.net>
2021-01-19 16:04:18 +0100

    use init container

--- prometheus/render.sh
+++ prometheus/render.sh
@@ -6,7 +6,14 @@ make clean prometheus_alerts.yaml prometheus_rules.yaml dashboards_out
 cd ..
 
 rm -rf rendered
-mkdir -p rendered
-mv kubernetes-mixin/prometheus_alerts.yaml rendered/
-mv kubernetes-mixin/prometheus_rules.yaml rendered/
-cp -vr kubernetes-mixin/dashboards_out/ rendered/dashboards
+mkdir -p rendered/dashboards
+cp kubernetes-mixin/prometheus_alerts.yaml rendered/
+cp kubernetes-mixin/prometheus_rules.yaml rendered/
+
+for i in $(find kubernetes-mixin/dashboards_out -type f); do 
+	bn="rendered/dashboards/$(basename $i)"
+	echo "Processing key $i to $bn"
+	cat $i | jq -c > $bn
+done
+
+tree rendered
