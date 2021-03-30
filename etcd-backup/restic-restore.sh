39b8cfd05d8ccf5a694f6a9bf7a1ec9a942509d9
Tomáš Kukrál <tom+noreply@6shore.net>
2021-03-30 17:43:09 +0200

    read creads from kubernetes

--- etcd-backup/restic-restore.sh
+++ etcd-backup/restic-restore.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
 export RESTIC_REPOSITORY="s3:http://$(dig +short @10.96.0.10 minio.minio.svc.cluster.local)/etcd-backup"
-export AWS_ACCESS_KEY_ID=glass
-export AWS_SECRET_ACCESS_KEY=paper123
-export RESTIC_PASSWORD=zuTeizaeng4Quuaheereofoh2yae5mael1aci3ieFuZaece0ezae7eePhoo4Iepi
+export AWS_ACCESS_KEY_ID=$(kubectl -n default get secret credentials -o json | jq -r .data.obj_username | base64 -d)
+export AWS_SECRET_ACCESS_KEY=$(kubectl -n default get secret credentials -o json | jq -r .data.obj_password | base64 -d)
+export RESTIC_PASSWORD=$(kubectl -n default get secret credentials -o json | jq -r .data.restic_password | base64 -d)
 
 restic cat config
 restic snapshots
