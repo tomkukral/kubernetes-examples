ed2954b15006deecf347c536072be5e87d824b62
Tomáš Kukrál <tom+noreply@6shore.net>
2021-03-30 17:21:11 +0200

    use local restore

--- etcd-backup/restic-restore.sh
+++ etcd-backup/restic-restore.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/bash -e
 
-export RESTIC_REPOSITORY="s3:http://minio.c0.k8s.6shore.net/etcd-backup"
+export RESTIC_REPOSITORY="s3:http://$(dig +short @10.96.0.10 minio.minio.svc.cluster.local)/etcd-backup"
 export AWS_ACCESS_KEY_ID=glass
 export AWS_SECRET_ACCESS_KEY=paper123
 export RESTIC_PASSWORD=zuTeizaeng4Quuaheereofoh2yae5mael1aci3ieFuZaece0ezae7eePhoo4Iepi
 
-restic cat config || exit 1
+restic cat config
 restic snapshots
 
 restic ls latest
