58058b8b909d5f3f5195ba63d9ead54aeebf7955
Tomáš Kukrál <noreply@6shore.net>
2023-10-17 10:25:17 +0200

    cilium enable hubble and wireguard

--- cilium/render-cilium.sh
+++ cilium/render-cilium.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-helm template --dry-run --values values.yml --namespace kube-system cilium/cilium
+helm template --dry-run --values values.yml --namespace kube-system --version 1.14.2 cilium/cilium
