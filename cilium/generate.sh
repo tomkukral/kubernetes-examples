52225124ce0378778ec23cf13173e032985216a8
Tomáš Kukrál <noreply@6shore.net>
2023-11-06 21:13:53 +0100

    cilium static render

---
+++ cilium/generate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for i in {0..2}; do 
+	helm template \
+	--dry-run --values values.yml \
+	--set cluster.name="c${i}" \
+	--set cluster.id="${i}" \
+	--set ipam.operator.clusterPoolIPv4PodCIDRList=10.${i}.0.0/17 \
+	--namespace kube-system --version 1.14.2 cilium/cilium > rendered-c${i}.yaml
+done
--- cilium/render-cilium.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-helm template --dry-run --values values.yml --namespace kube-system --version 1.14.2 cilium/cilium
