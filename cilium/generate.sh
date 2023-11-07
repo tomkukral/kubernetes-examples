5379e5be2052ad06fe124930d9bd137a7f09050f
Tomáš Kukrál <noreply@6shore.net>
2023-11-07 21:31:16 +0100

    add cilium content

--- cilium/generate.sh
+++ cilium/generate.sh
@@ -4,7 +4,7 @@ for i in {0..2}; do
 	helm template \
 	--dry-run --values values.yml \
 	--set cluster.name="c${i}" \
-	--set cluster.id="${i}" \
+	--set cluster.id="1${i}" \
 	--set ipam.operator.clusterPoolIPv4PodCIDRList=10.${i}.0.0/17 \
 	--namespace kube-system --version 1.14.2 cilium/cilium > rendered-c${i}.yml
 done
