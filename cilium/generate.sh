82992cdb0ef73c9fb7de4626af8d97816ca0eb16
Tomáš Kukrál <noreply@6shore.net>
2023-11-07 23:22:34 +0100

    updates

--- cilium/generate.sh
+++ cilium/generate.sh
@@ -6,6 +6,8 @@ for i in {0..2}; do
     --set cluster.name="c${i}" \
     --set cluster.id="1${i}" \
     --set ipam.operator.clusterPoolIPv4PodCIDRList=10.${i}.0.0/17 \
-    --set ipv4NativeRoutingCIDR=10.0.0.0/16 \
     --namespace kube-system --version 1.14.2 cilium/cilium > rendered-c${i}.yml
 done
+
+
+#  --set ipv4NativeRoutingCIDR=10.0.0.0/16 \
